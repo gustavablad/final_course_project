4ef7b2b4de189bb3a32407f8de655f3c46f973fd
gustavablad <gustav.ablad@icloud.com>
2026-05-18 22:06:41 +0000

    Made can_build_word and calculate_score more effective by eliminating usage of lists. Added some framework for Flask-app

--- scrabble-solver/app.py
+++ scrabble-solver/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 # if run in terminal: "$ pip --version" and it shows different version from python interpreter, change interpreter to pip's version
 from collections import Counter
 
+app = Flask(__name__)
+
 LETTER_POINTS = {
     "a": 1, "b": 3, "c": 3, "d": 2, "e": 1, 
     "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, 
@@ -17,29 +19,42 @@ with open ("scrabble-solver/dictionary.txt") as f:
 # set of all valid words in compliance with an online scrabble dictionary
 
 player_tiles = input("What tiles do you have? Use only the letters on the tiles and '*' for blank tiles. Do not use any commas or periods. Example of rack with 7 tiles: *egit*r. : ")
-# tile_count = Counter(tiles)
 
 def can_build_word(word, tiles):
-    
-    for letter in word:
-        remaining_tiles = list(player_tiles)
-        if letter in remaining_tiles:
-            remaining_tiles.remove(letter)
-        elif "*" in remaining_tiles:
-            remaining_tiles.remove("*")
-        else:
-            return False
+    tile_count = Counter(tiles)
+
+    blanks = tile_count["*"]
+    del tile_count["*"]
+
+    for letter, needed in Counter(word).items():
+        available = tile_count.get(letter, 0)
+
+        if needed > available:
+            missing = needed - available
+
+            if missing <= blanks:
+                blanks -= missing
+            else:
+                return False
+            
     return True
 
 def calculate_score(word, tiles):
+    tile_count = Counter(tiles)
+
+    blanks = tile_count["*"]
+    del tile_count["*"]
+    
     score = 0
+    
     for letter in word:
-        remaining_tiles = list(player_tiles)
-        if letter in remaining_tiles:
+        if tile_count.get(letter, 0) > 0:
             score += LETTER_POINTS[letter]
-            remaining_tiles.remove(letter)
-        elif "*" in remaining_tiles:
-            remaining_tiles.remove("*")
+            tile_count[letter] -= 1
+
+        elif blanks > 0:
+            blanks -= 1
+
     return score
 
 def find_words(tiles):
@@ -47,7 +62,13 @@ def find_words(tiles):
     for word in VALID_WORDS:
         if can_build_word(word, tiles):
             results.append({"word": word, "score": calculate_score(word, tiles), "length": len(word)})
+    
+    
     return results
 
 results = find_words(player_tiles)
 print(results)
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
